f96734cf8070b9b409a89876bc15c0fea9fafc07
tiya adugna <tiyakefalee@gmail.com>
2020-04-23 03:23:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
-
+class CashRegister
+ attr_accessor :total, :discount
+  
+  def initialize(discount=0)
+    @total = 0 
+    @discount = discount
+    
+  end
+  def add_item(title,price,quantity=1)
+    
+    
+  
+end
